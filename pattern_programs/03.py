f6eb9e097f79a0aa52d56895828ee72149805725
Sharath M <sharathm5684@gmail.com>
2024-01-24 19:57:10 +0530

    Full Pyramid Pattern

--- pattern_programs/03.py
+++ pattern_programs/03.py
@@ -1,9 +1,11 @@
-#left half pyramid pattern
+#Full Pyramid Pattern
 
 rows = 5
 for i in range(rows):
     for j in range(rows - i):
         print(" ", end = " ")
     for k in range(i + 1):
+        print("*", end = " ")
+    for m in range(i):
         print("*", end = " ")
     print()
