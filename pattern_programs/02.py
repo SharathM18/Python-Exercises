5a6138f96f9a858c046bab110532744235204831
Sharath M <sharathm5684@gmail.com>
2024-02-12 15:36:48 +0530

    modified programs 2 & 3

--- pattern_programs/02.py
+++ pattern_programs/02.py
@@ -2,7 +2,7 @@
 
 rows = 5
 for i in range(rows):
-    for j in range(rows - i):
+    for j in range(rows - (i + 1)):
         print(" ", end = " ")
     for k in range(i + 1):
         print("*", end = " ")
